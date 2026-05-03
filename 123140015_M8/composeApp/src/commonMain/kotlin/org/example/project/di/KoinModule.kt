52f38673cdea9b577ab146db4a16b891321e15cf
danarPrayogo <danar.123140015@student.itera.ac.id>
2026-05-03 19:21:50 +0700

    Remove week 8 folder

--- 123140015_M8/composeApp/src/commonMain/kotlin/org/example/project/di/KoinModule.kt
+++
@@ -1,5 +0,0 @@
-package org.example.project.di
-
-// Koin module is defined in androidMain/kotlin/org/example/project/di/KoinModule.kt
-// This file is kept for future cross-platform DI setup if needed
-
